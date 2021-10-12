55e29f9ec16da83230882d512cb5d9c6be7ca467
HIREP Noham <n.hirep@digi-lapa141.e-mazarine.home>
2021-10-12 16:39:04 +0200

    :bulb: Comments app.js
    Add of comments in app.js

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// Variable
 const inputCity = document.querySelector('.cityInput')
 const formSearch = document.querySelector('.formSearch')
 const btn = document.querySelector('.btnSearch')
@@ -8,12 +9,14 @@ const tempWheater = document.querySelector('.temp')
 const humidityWeater = document.querySelector('.humidity')
 
 btn.addEventListener('click', weather)
-
+// Function for have Weather of the city
 async function weather(e) {
   e.preventDefault()
     try {
       const city = inputCity.value
+      // Get the Data from the api
         const data = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&APPID=e77fa778cddc1c86d94bf65e5f487590`)
+      // Transform de the data in a Json
           const dataJason = await data.json() 
             console.log(dataJason);
               cityName.innerHTML = dataJason.name
